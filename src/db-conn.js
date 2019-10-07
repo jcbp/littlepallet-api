f124c8ea92ac5e3d993fec788b48165beba31b8b
cbertinci <jcbp.jcbp@gmail.com>
2019-10-07 19:13:21 -0300

    Fix code style

--- src/db-conn.js
+++ src/db-conn.js
@@ -5,19 +5,20 @@ const url = 'mongodb://localhost:27017';
 let client;
 
 module.exports = {
-    async open() {
-      try {
-        client = await MongoClient.connect(url, {useNewUrlParser: true});
-        const db = client.db('watercoon');
-        console.log('DB connection opened');
-        return db;
-      }
-      catch(err) {
-        throw new Error('Error connecting to DB', err);
-      }
-    },
-    close() {
-      client.close();
-      console.log('DB connection closed');
+  async open() {
+    try {
+      client = await MongoClient.connect(url, {useNewUrlParser: true});
+      const db = client.db('watercoon');
+
+      console.log('DB connection opened');
+      return db;
     }
+    catch(err) {
+      throw new Error('Error connecting to DB', err);
+    }
+  },
+  close() {
+    client.close();
+    console.log('DB connection closed');
+  }
 };
