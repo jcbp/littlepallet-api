b1c497ecfb56f9b84ea40c89af02e5538f851a31
cbertinci <jcbp.jcbp@gmail.com>
2019-10-07 19:09:56 -0300

    Fix indentation

--- data/db-init.js
+++ data/db-init.js
@@ -1,42 +1,49 @@
 const dbConn = require('../src/db-conn');
 
-(async () => {
+(async() => {
   const db = await dbConn.open();
+
+  await db.collection('lists').deleteMany();
   await db.collection('lists').insertMany([
     {
-        "name": "Tasks",
-        "fields": [
-            {
-                "type": "boolean",
-                "name": "Done"
-            },
-            {
-                "type": "text",
-                "name": "Title"
-            },
-            {
-                "type": "long-text",
-                "name": "Description"
-            },
-            {
-                "type": "options",
-                "options": ["Pepe", "Coco"],
-                "name": "Assign to"
-            },
-            {
-                "type": "time",
-                "name": "Time tracking"
-            }
-        ],
-        "items": [
-            [
-                false,
-                "Test task",
-                "This is a\nlooong descriptioooon",
-                "Coco",
-                "02 45 00"
-            ]
+      'name': 'Tasks',
+      'fields': [
+        {
+          'type': 'boolean',
+          'name': 'Done'
+        },
+        {
+          'type': 'date',
+          'name': 'Date'
+        },
+        {
+          'type': 'text',
+          'name': 'Title'
+        },
+        {
+          'type': 'long-text',
+          'name': 'Description'
+        },
+        {
+          'type': 'options',
+          'options': ['Pepe', 'Coco'],
+          'name': 'Assign to'
+        },
+        {
+          'type': 'time',
+          'name': 'Time tracking'
+        }
+      ],
+      'items': [
+        [
+          false,
+          '25 11 2019',
+          'Test task',
+          'This is a\nlooong descriptioooon',
+          'Coco',
+          '02 45 00'
         ]
+      ]
     }
   ]);
   dbConn.close();
