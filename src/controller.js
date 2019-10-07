527ad95e934a886d092c8b89c27a1cc28d1aaa89
cbertinci <jcbp.jcbp@gmail.com>
2019-10-07 19:10:19 -0300

    Log error

--- src/controller.js
+++ src/controller.js
@@ -5,11 +5,13 @@ module.exports = {
     try {
       const db = await dbConn.open();
       const lists = await db.collection('lists').find({}).toArray();
+
       dbConn.close();
       res.status(200).send(lists);
     }
-    catch (error) {
-      res.status(500).send(error);
+    catch(e) {
+      console.log(e);
+      res.status(500).send(e);
     }
   }
 };
