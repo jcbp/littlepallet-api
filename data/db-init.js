283a16bf30358c5d38ad34bfb3f86111aa2283ad
cbertinci <jcbp.jcbp@gmail.com>
2019-11-20 01:11:05 -0300

    Update model to use ids

--- data/db-init.js
+++ data/db-init.js
@@ -10,39 +10,65 @@ const dbConn = require('../src/db-conn');
       'fields': [
         {
           'type': 'boolean',
-          'name': 'Done'
-        },
-        {
-          'type': 'date',
-          'name': 'Date'
+          'name': 'Done',
+          'width': 90,
+          '_id': '0' 
         },
         {
           'type': 'text',
-          'name': 'Title'
+          'name': 'Title',
+          '_id': '2' 
+        },
+        {
+          'type': 'date',
+          'name': 'Date',
+          '_id': '1' 
         },
         {
           'type': 'long-text',
-          'name': 'Description'
+          'name': 'Description',
+          '_id': '3' 
         },
         {
           'type': 'options',
           'options': ['Pepe', 'Coco'],
-          'name': 'Assign to'
+          'name': 'Assign to',
+          '_id': '4' 
         },
         {
           'type': 'time',
-          'name': 'Time tracking'
+          'name': 'Time tracking',
+          '_id': '5' 
         }
       ],
       'items': [
-        [
-          false,
-          '25 11 2019',
-          'Test task',
-          'This is a\nlooong descriptioooon',
-          'Coco',
-          '02 45 00'
-        ]
+        {
+          '_id': '0',
+          '0': false,
+          '1': '2019-11-25',
+          '2': 'Test task',
+          '3': 'This is a\nlooong descriptioooon',
+          '4': 'Coco',
+          '5': '02:45:00'
+        },
+        {
+          '_id': '1',
+          '0': true,
+          '1': '2019-11-28',
+          '2': 'Other task',
+          '3': 'Lorem Ipsum',
+          '4': 'Pepe',
+          '5': '03:45:00'
+        },
+        {
+          '_id': '2',
+          '0': true,
+          '1': '2020-01-08',
+          '2': 'SAsasasa',
+          '3': 'Lorem Ipsum, dolor sit amet',
+          '4': 'Coco',
+          '5': '08:40:20'
+        }
       ]
     }
   ]);
