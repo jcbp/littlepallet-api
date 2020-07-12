84f5d014b8468b56651e2ce30ce85f64b085d307
cbertinci <jcbp.jcbp@gmail.com>
2020-07-12 20:48:35 -0300

    change access according to staging

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 const allowCrossDomain = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://0.0.0.0:9000');
+  // res.header('Access-Control-Allow-Origin', 'http://0.0.0.0:9000');
+  res.header('Access-Control-Allow-Origin', 'littlepallet.herokuapp.com');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
--- src/db-conn.js
+++ src/db-conn.js
@@ -1,6 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
 
-const url = 'mongodb://localhost:27017';
+const urlLocal = 'mongodb://localhost:27017';
+const url = 'mongodb+srv://adminDbUser:Np8lc88rTrNXNUDQ@cluster0.adroc.mongodb.net/littlepallet?retryWrites=true&w=majority';
 
 let client;
 
@@ -8,7 +9,7 @@ module.exports = {
   async open() {
     try {
       client = await MongoClient.connect(url, {useNewUrlParser: true});
-      const db = client.db('watercoon');
+      const db = client.db('littlepallet');
 
       console.log('DB connection opened');
       return db;
