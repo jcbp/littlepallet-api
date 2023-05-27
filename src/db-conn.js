3fad28bde1cf3328831b94db1cbe20e5a160367e
Carlos Bertinci <cbertinci@vi-datec.com>
2023-05-26 22:39:56 -0300

    fix: remove connection string from code

--- src/db-conn.js
+++ src/db-conn.js
@@ -1,8 +1,6 @@
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require("mongodb").MongoClient;
 
-const url = process.env.NODE_ENV === 'development'
-  ? 'mongodb://localhost:27017'
-  : 'mongodb+srv://adminDbUser:Np8lc88rTrNXNUDQ@cluster0.adroc.mongodb.net/littlepallet?retryWrites=true&w=majority';
+const url = process.env.CONNECTION_STR;
 
 let client;
 let db;
@@ -15,39 +13,33 @@ module.exports = {
         // retry to connect for 60 times
         reconnectTries: 60,
         // wait 1 second before retrying
-        reconnectInterval: 1000
+        reconnectInterval: 1000,
       });
-      db = client.db('littlepallet');
+      db = client.db("littlepallet");
 
-      console.log('DB connection opened');
-    }
-    catch(err) {
-      throw new Error('Error connecting to DB', err);
+      console.log("DB connection opened");
+    } catch (err) {
+      throw new Error("Error connecting to DB", err);
     }
   },
   async close() {
     await client.close();
-    console.log('DB connection closed');
+    console.log("DB connection closed");
   },
   async withTransaction(asyncCallback) {
     const session = await client.startSession();
     const transactionOptions = {
-      readPreference: 'primary',
-      readConcern: { level: 'local' },
-      writeConcern: { w: 'majority' }
+      readPreference: "primary",
+      readConcern: { level: "local" },
+      writeConcern: { w: "majority" },
     };
     try {
-      await session.withTransaction(
-        asyncCallback,
-        transactionOptions
-      );
-    }
-    catch(e) {
+      await session.withTransaction(asyncCallback, transactionOptions);
+    } catch (e) {
       throw new Error(
-        'The transaction was aborted due to an unexpected error: ' + e
+        "The transaction was aborted due to an unexpected error: " + e
       );
-    }
-    finally {
+    } finally {
       await session.endSession();
     }
   },
@@ -56,5 +48,5 @@ module.exports = {
   },
   getCollection(collectionName) {
     return db.collection(collectionName);
-  }
+  },
 };
